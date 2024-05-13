0748a78db57e426a0b3679786ee4410b00a1e71a
effiongcharles <talkwithcharles@gmail.com>
2024-05-13 17:39:19 +0200

    updates

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 To run: python3 -m app
+
+# About project
+ This is a movie project using Python flask, js, css, html5, bootstrap framework. No backend databased, only a local json file as a db. 
+
+## The following operations are persisted:
+1. Adding a new movie
+2. Updating a movie
+
+## Delete operating is not persisted. 
+
+I created this project in order to demonstrate a dynamic web application to Software Engineering Students at Masterschool. 
+
+The project is meant to show what's possible with Python Flask/HTML/CSS. Also to serve as something for them to look forward to. 
--- app.py
+++ app.py
@@ -1,93 +1,43 @@
 from flask import Flask, render_template, request, redirect
 import random
 import statistics
+import os
+import json
 
 app = Flask(__name__)
 
-MOVIES = [
-    {
-        "id": 1,
-        "title": "The Shawshank Redemption",
-        "rating": 5,
-        "image": "https://cdn.pixabay.com/photo/2017/08/10/00/52/house-2616607_1280.jpg"
-    },
-    {
-        "id": 2,
-        "title": "Pulp Fiction",
-        "rating": 1,
-        "image": "https://cdn.pixabay.com/photo/2017/11/24/05/38/jet-2974131_1280.jpg"
-    },
-    {
-        "id": 3,
-        "title": "The Room",
-        "rating": 2,
-        "image": "https://cdn.pixabay.com/photo/2018/10/26/11/14/man-3774381_1280.jpg"
-    },
-    {
-        "id": 4,
-        "title": "The Godfather",
-        "rating": 5,
-        "image": "https://cdn.pixabay.com/photo/2016/09/22/13/51/bollywood-1687410_1280.jpg"
-    },
-    {
-        "id": 5,
-        "title": "The Godfather: Part II",
-        "rating": 4,
-        "image": "https://cdn.pixabay.com/photo/2017/09/16/01/31/charlie-chaplin-2754312_1280.jpg"
-    },
-    {
-        "id": 6,
-        "title": "The Dark Knight",
-        "rating": 3,
-        "image": "https://cdn.pixabay.com/photo/2015/09/25/18/47/tunnel-957963_1280.jpg"
-    },
-    {
-        "id": 7,
-        "title": "Everything Everywhere All At Once",
-        "rating": 3,
-        "image": "https://cdn.pixabay.com/photo/2016/03/27/07/38/police-1282330_1280.jpg"
-    },
-    {
-        "id": 8,
-        "title": "12 Angry Men",
-        "rating": 3,
-        "image": "https://cdn.pixabay.com/photo/2015/09/03/17/28/man-921004_1280.jpg"
-    },
-    {
-        "id": 9,
-        "title": "Forrest Gump",
-        "rating": 2,
-        "image": "https://cdn.pixabay.com/photo/2018/02/13/23/41/nature-3151869_1280.jpg"
-    },
-    {
-        "id": 10,
-        "title": "Star Wars: Episode V",
-        "rating": 4,
-        "image": "https://cdn.pixabay.com/photo/2014/10/02/06/34/war-469503_1280.jpg"
-    },
-]
+def read_file(path=os.path.abspath("movies.json")):
+    with open(path, "r") as f:
+        data = json.load(f)
+    return data
+
+def write_file(data, path=os.path.abspath("movies.json")):
+    json_str = json.dumps(data)
+    with open(path, "w") as f: 
+        f.write(json_str)
 
 @app.route("/")
 def index():
-    # return first 4 movies
-    movies = []
+    # return first 4 movie
+    movies = read_file(os.path.abspath("movies.json"))
     for i in range(4):
-        movies.append(MOVIES[i])
+        movies.append(movies[i])
 
     return render_template('index.html', movies = movies)
 
 
 @app.route("/movies", methods =["GET"])
 def movie():
-    return render_template('movie.html', movies = MOVIES)
+    movies = read_file(os.path.abspath("movies.json"))
+    return render_template('movie.html', movies = movies)
         
 
 @app.route("/movies/search", methods =["POST"])
 def search_movies():
     title = request.form['title']
     if title:
-        movies = []
-        for movie in MOVIES:
+        movies = read_file(os.path.abspath("movies.json"))
+        for movie in movies:
             if title.lower() in movie["title"].lower():
                 movies.append(movie)
         return render_template("movie.html", movies=movies)
@@ -97,18 +47,21 @@ def search_movies():
 @app.route("/movies/random")
 def random_movie():
     i = random.randint(0, 9)
-    movie = MOVIES[i]
+    movies = read_file(os.path.abspath("movies.json"))
+    movie = movies[i]
     print(movie)
     return render_template('random.html', movie = movie, title="Tonight's movie")
 
 @app.route("/movies/sort")
 def sort_movies():
-    movies = sorted(MOVIES, key=lambda d: d['rating'], reverse=True)
+    movies = read_file(os.path.abspath("movies.json"))
+    movies = sorted(movies, key=lambda d: d['rating'], reverse=True)
     return render_template('movie.html', movies = movies, sort_desc="sort")
 
 @app.route("/movies/stats")
 def movies_stats():
-    movies = sorted(MOVIES, key=lambda d: d['rating'], reverse=True)
+    movies = read_file(os.path.abspath("movies.json"))
+    movies = sorted(movies, key=lambda d: d['rating'], reverse=True)
 
     # Get all the ratings
     ratings = []
@@ -122,56 +75,73 @@ def movies_stats():
 
 @app.route("/movies/delete/<int:id>")
 def delete(id):
-    for i in range(len(MOVIES)):
-        if id == MOVIES[i].get("id"):
-            del MOVIES[i]
+    movies = read_file(os.path.abspath("movies.json"))
+    for i in range(len(movies)):
+        if id == movies[i].get("id"):
+            del movies[i]
             break
-    return render_template('movie.html', movies = MOVIES)
+    return render_template('movie.html', movies = movies)
 
 
 @app.route("/movies/add", methods=["GET", "POST"])
 def add_movie():
     if request.method == "GET":
-        return render_template("movie.html", add_form="add_form", movies=MOVIES)
+        movies = read_file(os.path.abspath("movies.json"))
+        return render_template("movie.html", add_form="add_form", movies=movies)
     else:
+        movies = read_file(os.path.abspath("movies.json"))
+        
         # Get new movie details
         title = request.form['title']
         image = request.form['image']
         rating = int(request.form['rating'])
 
         movie = {
-            "id": len(MOVIES) + 1,
+            "id": len(movies) + 1,
             "title": title, 
             "image": image,
             "rating": rating
         }
 
+        if title != "" and image != "" and rating != "":
+            movies.insert(0, movie)
 
-        MOVIES.insert(0, movie)
+            # save the file
+            write_file(movies, os.path.abspath("movies.json"))
+
         return redirect("/movies")
+
+
+
 
 @app.route("/movies/edit/<int:id>", methods=['GET', 'POST'])
 def edit_form(id):
     if request.method == "GET":
+        movies = read_file(os.path.abspath("movies.json"))
+
         # Find the movie data
         movie = {}
-        for i in range(len(MOVIES)):
-            if id == MOVIES[i].get("id"):
-                movie = MOVIES[i]
+        for i in range(len(movies)):
+            if id == movies[i].get("id"):
+                movie = movies[i]
                 break
-        return render_template('movie.html', movies = MOVIES, movie = movie)
+        return render_template('movie.html', movies = movies, movie = movie)
     else:
+        movies = read_file(os.path.abspath("movies.json"))
+
         title = request.form['title']
         image = request.form['image']
         rating = int(request.form['rating'])
 
         # find the movie and update the values
-        for i in range(len(MOVIES)):
-            if id == MOVIES[i].get("id"):
-                MOVIES[i]["title"] = title
-                MOVIES[i]["image"] = image
-                MOVIES[i]["rating"] = rating
-                break
+        for i in range(len(movies)):
+            if id == movies[i].get("id"):
+                movies[i]["title"] = title
+                movies[i]["image"] = image
+                movies[i]["rating"] = rating
+                
+                # save the updates
+                write_file(movies, os.path.abspath("movies.json"))
         return redirect("/movies")
 
 if __name__ == "__main__":
